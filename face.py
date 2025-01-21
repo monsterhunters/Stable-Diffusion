c0e1b25a533a201d901034a6f19e4f40b49011e3
kyawkaung <88552297+monsterhunters@users.noreply.github.com>
2025-01-22 02:19:15 +0630

    Update face.py

--- face.py
+++ face.py
@@ -1,5 +1,7 @@
 import os
 import shutil
+
 os.system('git clone https://github.com/facefusion/facefusion.git')
 shutil.move("facefusion", "face")
 os.chdir('face')
+os.rename('facefusion.py', 'run.py')
